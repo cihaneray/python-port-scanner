6e0be9fdacb30c5d54b78a6afa4bdc94aa68e849
Cihan Eray <91616342+cihaneray@users.noreply.github.com>
2025-04-22 00:11:46 +0300

    Creating & Adding PythonPortScanner.py

---
+++ PythonPortScanner.py
@@ -0,0 +1,51 @@
+#! /usr/bin/python3
+
+import argparse
+import threading
+from argparse import ArgumentParser, Namespace
+
+from queue import Queue
+from socket import AF_INET, SOCK_STREAM, gethostbyname, socket
+
+
+class PortScanner:
+    def __init__(self) -> None:
+        self.queue: Queue = Queue()
+        self.set_parser()
+
+    def set_parser(self) -> None:
+        parser: ArgumentParser = argparse.ArgumentParser(description='TCP Port Scanner')
+        parser.add_argument('host', help='Host to scan')
+        parser.add_argument('ports', help='Port range to scan, formatted as start-end')
+        args: Namespace = parser.parse_args()
+        start_port, end_port = map(int, args.ports.split('-'))
+        self.main(args.host, start_port, end_port)
+
+    @staticmethod
+    def tcp_test(port: int, target_ip: str) -> None:
+        with socket(AF_INET, SOCK_STREAM) as sock:
+            sock.settimeout(1)
+            result: int = sock.connect_ex((target_ip, port))
+            if result == 0:
+                print(f"Opened Port: {port}")
+
+    def worker(self, target_ip: str, queue: Queue) -> None:
+        while not queue.empty():
+            port = queue.get()
+            self.tcp_test(port, target_ip)
+            queue.task_done()
+
+    def main(self, host: str, start_port: int, end_port: int) -> None:
+        target_ip = gethostbyname(host)
+        for port in range(start_port, end_port + 1):
+            self.queue.put(port)
+        for _ in range(100):
+            t = threading.Thread(target=self.worker, args=(target_ip, self.queue,))
+            t.daemon = True
+            t.start()
+        self.queue.join()
+        print("Scanning completed.")
+
+
+if __name__ == '__main__':
+   PortScanner()

06a7d1ffd26875baa6b95de1d4e110166fb5aa10
cihaneray <91616342+cihaneray@users.noreply.github.com>
2025-05-05 23:47:40 +0300

    Added banner.

--- port_scanner.py
+++ port_scanner.py
@@ -91,6 +91,17 @@ class PortScanner:
         # Load common ports from config file
         self.common_ports = self.load_port_config()
 
+        print("""
+           C8888DC8888DC8888DC8888DC8888DC8888DC8888DC8888DC8888DC8888DC8888DC8888DCb
+          dD                d8888b. .d8888.  .o88b.  .d8b.  d8b   db                Cb            
+         d8'                88  `8D 88'  YP d8P  Y8 d8' `8b 888o  88                `8b           
+        d8'                 88oodD' `8bo.   8P      88ooo88 88V8o 88                 `8b          
+       d8'                  88~~~     `Y8b. 8b      88~~~88 88 V8o88                  `8b         
+      d8'                   88      db   8D Y8b  d8 88   88 88  V888                   `8b        
+     C8'                    88      `8888Y'  `Y88P' YP   YP VP   V8P                    `8D       
+    C88DC8888DC8888DC8888DC8888DC8888DC8888DC8888DC8888DC8888DC8888DC8888DC8888DC8888DC8888D 
+""")
+
         self.scan()
 
     @staticmethod

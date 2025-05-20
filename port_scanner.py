da10bf14acc23f322a67bd1f83a273d5103e923c
cihaneray <91616342+cihaneray@users.noreply.github.com>
2025-05-20 19:31:18 +0300

    Updated README.md

--- port_scanner.py
+++ port_scanner.py
@@ -998,3 +998,6 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         print("\nScan aborted by user")
         sys.exit(0)
+    except EOFError:
+        print("\nScan aborted by user")
+        sys.exit(0)
